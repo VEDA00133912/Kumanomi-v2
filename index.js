e5d75d7874d93881835f4202b3a1facd566c1579
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-03 05:46:41 +0000

    最初の構成だけ

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { loadCommands, loadAdminCommands, loadEvents } = require('./module/event/other/loader');
+const setupGlobalErrorHandling = require('./error/global');
+const path = require('path');
+require('dotenv').config();
+
+const client = new Client(
+    { 
+        intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent, 
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences
+     ]
+    }
+);
+
+client.commands = new Collection();
+client.contextMenuCommands = new Collection();
+
+loadCommands(client, path.join(__dirname, 'module/function/slashcommand'));
+loadCommands(client, path.join(__dirname, 'module/function/contextmenu'));
+loadAdminCommands(client, path.join(__dirname, 'module/function/admin'));
+loadEvents(client, path.join(__dirname, 'module/event'));
+
+setupGlobalErrorHandling(client);
+client.login(process.env.TOKEN);
