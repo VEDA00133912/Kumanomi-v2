976d0a568d875eb8ed13df10ab246cf62c374bb4
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-10 00:32:35 +0000

    npmコマンドを追加

---
+++ module/lib/npm.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+async function getPackageInfo(packageName) {
+  try {
+    const { data } = await axios.get(`https://registry.npmjs.org/${packageName}`);
+    const latestVersion = data['dist-tags'].latest;
+    const { license, author, homepage, repository } = data.versions[latestVersion];
+
+    return {
+      name: data.name,
+      version: latestVersion,
+      license: license || '不明',
+      author: author?.name || '不明',
+      homepage: homepage || 'なし',
+      repository: repository?.url || 'なし',
+      lastPublish: new Date(data.time[latestVersion]).toLocaleString('ja-JP')
+    };
+  } catch {
+    throw new Error('<:error:1302169165905526805> 指定されたパッケージが見つかりませんでした');
+  }
+}
+
+module.exports = { getPackageInfo };
