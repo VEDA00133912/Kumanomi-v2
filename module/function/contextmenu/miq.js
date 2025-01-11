058bf5e32ea7e4fcae4f878eed54470151106af2
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-11 02:46:30 +0000

    Make it a Quoteの追加

---
+++ module/function/contextmenu/miq.js
@@ -0,0 +1,34 @@
+const { ApplicationCommandType, ContextMenuCommandBuilder, AttachmentBuilder } = require('discord.js');
+const { MiQ } = require('makeitaquote');
+const cooldown = require('../../event/other/cooldown');
+const contextmenuError = require('../../../error/contextmenu');
+const { createEmbed } = require('../../lib/embed');
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName('Make it a Quote')
+    .setType(ApplicationCommandType.Message),
+
+  async execute(interaction) {
+    if (cooldown(this.data.name, interaction)) return;
+
+    await interaction.deferReply();
+        const msg = interaction.targetMessage;
+        const miq = new MiQ()
+        .setFromMessage(msg)
+        .setWatermark(interaction.client.user.username);
+
+    try {
+      const response = await miq.generateBeta();
+      const attachment = new AttachmentBuilder(response, { name: 'miq.png' });
+
+      const embed = createEmbed(interaction)
+        .setDescription(`**[元メッセージへ飛ぶ🕊️](${msg.url})**`)
+        .setImage('attachment://miq.png');
+
+      await interaction.editReply({ embeds: [embed], files: [attachment] });
+    } catch (error) {
+      contextmenuError(interaction.client, interaction, error);
+    }
+  },
+};
