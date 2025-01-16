d301df76a54d4b4e82668227227e6d7521b604a3
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-16 03:07:33 +0000

    mongoDBの設定とexpand-settings追加

---
+++ module/function/slashcommand/setting/expand.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder, InteractionContextType, PermissionFlagsBits, EmbedBuilder, MessageFlags, Colors } = require('discord.js');
+const cooldown = require('../../../event/other/cooldown');
+const slashcommandError = require('../../../../error/slashcommand');
+const Settings = require('../../../../file/setting/mongodb'); 
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('expand-settings')
+        .setDescription('メッセージリンクの展開をオンまたはオフにします。')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .setContexts(InteractionContextType.Guild)
+        .setIntegrationTypes(0)
+        .addStringOption(option =>
+            option.setName('on-off')
+                .setDescription('オンかオフを選択')
+                .setRequired(true)
+                .addChoices(
+                    { name: '展開機能ON', value: 'true' },
+                    { name: '展開機能OFF', value: 'false' },
+                )),
+    
+    async execute(interaction) {
+        if (cooldown(this.data.name, interaction)) return;
+
+        const status = interaction.options.getString('on-off') === 'true';
+        
+        try {
+            await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+            let settings = await Settings.findOne({ guildId: interaction.guild.id });
+
+            if (!settings) {
+                settings = new Settings({
+                    guildId: interaction.guild.id,
+                    expand: status,
+                });
+            } else if (settings.expand === status) {
+                const embed = new EmbedBuilder()
+                    .setTitle('設定変更')
+                    .setDescription(`<:error:1302169165905526805> すでに${status ? 'ON' : 'OFF'}になっています。`)
+                    .setColor(status ? Colors.Green : Colors.Red)  
+                    .setFooter({ text: 'Kumanomi | expand', iconURL: interaction.client.user.displayAvatarURL() })
+                    .setTimestamp();
+
+                return interaction.editReply({ embeds: [embed] });
+            }
+
+            settings.expand = status;
+            await settings.save();
+
+            const embed = new EmbedBuilder()
+                .setDescription(`<:check:1302169183110565958> **メッセージリンクの展開は ${status ? 'ON' : 'OFF'} になりました。**`)
+                .setColor(status ? Colors.Green : Colors.Red)  
+                .setFooter({ text: 'Kumanomi | expand', iconURL: interaction.client.user.displayAvatarURL() })
+                .setTimestamp();
+
+            await interaction.editReply({ embeds: [embed] });
+        } catch (error) {
+            slashcommandError(interaction.client, interaction, error);
+        }
+    },
+};
