fe95dffa8d7989480de9ad863b06e20c1348cfcc
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-18 05:30:26 +0000

    ちょっと表記修正

--- file/setting/mongodb.js
+++ file/setting/mongodb.js
@@ -1,40 +1,53 @@
 const mongoose = require('mongoose');
 
 const expandSchema = new mongoose.Schema({
-    guildId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    expand: {
-        type: Boolean,
-        default: false,
-    },
+	guildId: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	expand: {
+		type: Boolean,
+		default: false,
+	},
 });
 
 const omikujiSchema = new mongoose.Schema({
-    userId: { 
-        type: String, 
-        required: true, 
-        unique: true 
-    },
-    result: { 
-        type: String, 
-        required: true 
-    },
-    date: { 
-        type: Date, 
-        default: Date.now 
-    },
+	userId: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	result: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	},
 });
 
 const timerSchema = new mongoose.Schema({
-    userId: { type: String, required: true, unique: true },
-    channelId: { type: String, required: true },
-    timeLeft: { type: Number, required: true },
-    startTime: { type: Number, required: true },
-  });
-  
+	userId: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	channelId: {
+		type: String,
+		required: true
+	},
+	timeLeft: {
+		type: Number,
+		required: true
+	},
+	startTime: {
+		type: Number,
+		required: true
+	},
+});
+
 const Timer = mongoose.model('Timer', timerSchema);
 const Expand = mongoose.model('Expand', expandSchema);
 const Omikuji = mongoose.model('Omikuji', omikujiSchema);
