049df026fbcc4bcd3d3b41a6f839afc624a84f36
りょう <143770907+VEDA00133912@users.noreply.github.com>
2025-01-21 01:31:07 +0000

    メッセージリンクの展開機能実装

--- file/setting/mongodb.js
+++ file/setting/mongodb.js
@@ -1,55 +1,72 @@
 const mongoose = require('mongoose');
 
 const expandSchema = new mongoose.Schema({
-	guildId: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	expand: {
-		type: Boolean,
-		default: false,
-	},
+  guildId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expand: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const blacklistSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  guildId: {
+    type: String,
+    required: true,
+  },
+  addedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const omikujiSchema = new mongoose.Schema({
-	userId: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	result: {
-		type: String,
-		required: true
-	},
-	date: {
-		type: Date,
-		default: Date.now
-	},
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  result: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const timerSchema = new mongoose.Schema({
-	userId: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	channelId: {
-		type: String,
-		required: true
-	},
-	timeLeft: {
-		type: Number,
-		required: true
-	},
-	startTime: {
-		type: Number,
-		required: true
-	},
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  channelId: {
+    type: String,
+    required: true,
+  },
+  timeLeft: {
+    type: Number,
+    required: true,
+  },
+  startTime: {
+    type: Number,
+    required: true,
+  },
 });
 
-const Timer = mongoose.model('Timer', timerSchema);
 const Expand = mongoose.model('Expand', expandSchema);
+const Blacklist = mongoose.model('Blacklist', blacklistSchema);
 const Omikuji = mongoose.model('Omikuji', omikujiSchema);
+const Timer = mongoose.model('Timer', timerSchema);
 
-module.exports = { Expand, Omikuji, Timer };
+module.exports = { Expand, Blacklist, Omikuji, Timer };
